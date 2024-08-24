188189e27e062d394152699f716cd8bb4c75f411
MichalRotkop <shalvi.r@gmail.com>
2024-08-24 15:55:07 +0300

    pulseList small edit

--- src/cmps/pulse/PulseList.jsx
+++ src/cmps/pulse/PulseList.jsx
@@ -1,13 +1,13 @@
 import { PulsePreview } from "./PulsePreview";
 
-export function PulseList({ group, onUpdatePulse }) {
+export function PulseList({ group }) {
 
     return (
         <section className="pulses-container">
             <ul className="pulse-list">
                 {group.pulses.map(pulse =>
                     <li className="pulse" key={pulse.id}>
-                        <PulsePreview pulse={pulse} group={group} onUpdatePulse={onUpdatePulse} />
+                        <PulsePreview pulse={pulse} group={group} />
                     </li>)
                 }
             </ul>
