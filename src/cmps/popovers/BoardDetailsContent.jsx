bf63c49ab61ff70841e2dc8a1a7d2374769cb4da
ShaharGdn <shahargadon@gmail.com>
2024-09-07 15:47:32 +0300

    Functional Starring on BoardHeader

--- src/cmps/popovers/BoardDetailsContent.jsx
+++ src/cmps/popovers/BoardDetailsContent.jsx
@@ -1,43 +1,44 @@
+import { useState } from "react";
 import { FaRegStar, FaStar } from "react-icons/fa6";
+import { updateBoard } from "../../store/actions/board.actions";
+import { showErrorMsg, showSuccessMsg } from "../../services/event-bus.service";
 
 
 export function BoardDetailsContent({ board }) {
+    const [boardToEdit, setBoardToEdit] = useState(board)
+
+    async function onUpdateBoard(boardToUpdate) {
+        try {
+            const board = await updateBoard(boardToUpdate)
+            setBoardToEdit({ ...board })
+            showSuccessMsg('Board Updated Successfully')
+        } catch (err) {
+            console.log('err: Cannot Update board', err)
+            showErrorMsg('Cannot Update Board')
+        }
+    }
+
+    function onFavoriteBoard(event) {
+        event.preventDefault()
+        event.stopPropagation()
+
+        const boardToUpdate = {
+            ...boardToEdit, 'isStarred': boardToEdit.isStarred ? false : true
+        }
+        onUpdateBoard(boardToUpdate)
+    }
     return (
         <div className="board-details-popover">
             <div className="board-title">
                 <h3>{board.title}</h3>
-                <div className="toggle-favorite">
-                    {!board.isStarred ?
+                <div className="toggle-favorite" onClick={onFavoriteBoard}>
+                    {boardToEdit.isStarred ?
                         <FaStar color="#ffcb00" size={19} /> :
                         <FaRegStar size={19} />}
                 </div>
             </div>
             <p>Manage any type of project. Assign owners, set timelines and keep track of where your project stands.</p>
             <div>Board info</div>
-            {/* <div>
-                <div>Board type</div>
-                <span>Main</span>
-            </div>
-            <div>
-                <div>Owner</div>
-                <div>
-                    <img
-                        alt="Shahar Bussines"
-                        src="https://files.monday.com/euc1/photos/65494569/thumb_small/65494569-user_photo_initials_2024_08_31_08_06_29.png?1725091589"
-                    />
-                    <div>Shahar Bussines</div>
-                </div>
-            </div>
-            <div>
-                <div>Created by</div>
-                <button>
-                    <img
-                        src="https://files.monday.com/euc1/photos/65494569/thumb/65494569-user_photo_initials_2024_08_31_08_06_29.png?1725091589"
-                        className="person-bullet-image"
-                    />
-                    on Aug 31, 2024
-                </button>
-            </div> */}
         </div>
     );
 }
