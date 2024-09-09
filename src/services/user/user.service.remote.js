a9a2515c73465ddef93511dae7d8b396d2c4de16
MichalRotkop <shalvi.r@gmail.com>
2024-09-09 16:28:50 +0300

    guest img update

--- src/services/user/user.service.remote.js
+++ src/services/user/user.service.remote.js
@@ -43,7 +43,7 @@ async function login(userCred) {
 }
 
 async function signup(userCred) {
-	if (!userCred.imgUrl) userCred.imgUrl = 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png'
+	if (!userCred.imgUrl) userCred.imgUrl = 'https://cdn.pixabay.com/photo/2017/07/18/23/23/user-2517433_1280.png'
 
     const user = await httpService.post('auth/signup', userCred)
 	return saveLoggedinUser(user)
